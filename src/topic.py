eba6fc6975465bddaa8ad70c367076aabf46b986
Anderson <andersonguchoa@gmail.com>
2022-11-07 10:17:47 -0300

    initial commit

---
+++ src/topic.py
@@ -0,0 +1,27 @@
+from src.passageiro import Passageiro
+
+
+class Topic:
+    def __init__(self, capacidade: int, qtdPrioritarios):
+        pass
+
+    def getNumeroAssentosPrioritarios(self):
+        return 0
+
+    def getNumeroAssentosNormais(self):
+        return 0
+
+    def getPassageiroAssentoNormal(self, lugar):
+        return None
+
+    def getPassageiroAssentoPrioritario(self, lugar):
+        return None
+
+    def getVagas(self):
+        return -1
+
+    def subir(self, passageiro: Passageiro):
+        return False
+
+    def descer(self, nome):
+        return True
