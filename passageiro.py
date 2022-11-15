80c8e6db3062c5a0ce7c294bc20f0ea3d8177d42
cauaa23 <116275269+cauaa23@users.noreply.github.com>
2022-11-14 23:13:50 -0300

    Create passageiro.py

---
+++ passageiro.py
@@ -0,0 +1,13 @@
+lasse  Passageiro :
+    def  _init_ ( self , nome : str , idade : int ):
+        próprio . nome  =  nome
+        próprio . idade  =  idade
+
+    def  ePrioridade ( self ):
+        se  auto . idade  >=  65 :
+            retornar  Verdadeiro
+        mais :
+            retornar  Falso
+
+    def  getNome ( self ):
+        retornar  a si mesmo . nome
